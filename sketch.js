fce9a9c7a5b7c5027ea512112bf521a4ef8b191d
Maísa Grasiele <marcelino.maisa@escola.pr.gov.br>
2024-09-18 21:19:15 -0300

    adicionando arquivos css e js

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(600, 600);
+    background("white");
+  }
+  
+  function draw() {
+    stroke("purple");
+    fill("pink");
+  
+    //console.log(mouseIsPressed)
+    
+    if (mouseIsPressed) {
+      rect(mouseX, mouseY, 37, 35);
+    }
